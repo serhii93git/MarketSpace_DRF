67fb5c6773bc8848d76323564b227c25096c9094
serhii <demchenko.s@hotmail.com>
2023-12-15 15:12:45 +0200

    create model for Product

--- marketSpace/products/models.py
+++ marketSpace/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=225)
+    description = models.TextField(max_length=3000)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=False)
+    contacts = models.TextField(max_length=500)
+
+    prod_image = models.ManyToManyField(to='Image', blank=True, null=True, related_name='prod_image')
+    category = models.ForeignKey(to='Categories', on_delete=models.PROTECT, related_name='category')
+    created_by = models.ForeignKey(to='Users', on_delete=models.CASCADE, related_name='created_by')
+
+    def __str__(self):
+        return f"{self.title} - {self.description}"
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='prod_image/', blank=True, null=True)
+    description = models.TextField(blank=True)
