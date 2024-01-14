5c3eba0b35977409398525940452ea142104d806
Serhii-1993 <149478861+Serhii-1993@users.noreply.github.com>
2024-01-14 15:40:29 +0200

    Update models.py

--- marketSpace/comments/models.py
+++ marketSpace/comments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from users.models import Users
-from products.models import Product
+
 
 
 class Comments(models.Model):
@@ -9,7 +9,7 @@ class Comments(models.Model):
     parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='replies')
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
+    product = models.ForeignKey(to='products.Product', on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
 
     def __str__(self):
         return f'{self.user.username} : {self.text[:50]}'
