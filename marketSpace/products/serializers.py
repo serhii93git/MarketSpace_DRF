b924919f1fe0deb4006a50ffe335d3b1f9856a2d
Serhii-1993 <149478861+Serhii-1993@users.noreply.github.com>
2024-01-15 11:27:55 +0200

    Update serializers.py

--- marketSpace/products/serializers.py
+++ marketSpace/products/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from .models import Product, Image
-from comments.serializers import CommentSerializer 
+ 
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    comments = CommentSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Product
         fields = '__all__'
