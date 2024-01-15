3c833446502a8fba9383588003e8c58e9ab72b9c
Serhii-1993 <149478861+Serhii-1993@users.noreply.github.com>
2024-01-15 11:48:36 +0200

    Update urls.py

--- marketSpace/products/urls.py
+++ marketSpace/products/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('prod/', ProductsListView.as_view(), name='prod-list'),
     # path('prod-add/', ProductAddView.as_view(), name='prod-add'),
     path('prod/<int:pk>', ProductDetailView.as_view(), name='prod-detail'),
-    path('img/', ImageAddView.as_view(), name='many-img-add-for-prod'),
-    path('image/', ImageListView.as_view(), name='imahe-list'),
-    path('image/<int:pk>/', ImageDetailView.as_view(), name='image-detail'),
+    # path('img/', ImageAddView.as_view(), name='many-img-add-for-prod'),
+    # path('image/', ImageListView.as_view(), name='imahe-list'),
+    # path('image/<int:pk>/', ImageDetailView.as_view(), name='image-detail'),
 
 ]
